000946c5776996eb251207f091c54cce0cdecff0
BrigitteSprung <brigittesprung@gmail.com>
2017-02-23 23:32:09 +0200

    playing with the optimiser

--- src/HashCache.java
+++ src/HashCache.java
@@ -4,11 +4,12 @@ public class HashCache {
     public int size; // The size of the cache in MB.
     public int ID;
     public ArrayList<HashVideo> videos = new ArrayList<>();
+    public int fullness;
 
     public HashCache(int id, int s) {
         this.ID = id;
         this.size = s;
-
+        this.fullness = 0;
     }
 
     public void setVideos(ArrayList<HashVideo> vids) {
--- src/HashOptimiser.java
+++ src/HashOptimiser.java
@@ -7,7 +7,20 @@ public class HashOptimiser {
                             ArrayList<HashVideo> vid,
                             ArrayList<HashEndpoint> ends
                         ){
+        int currentCache = 0;
+        int total =0;
+        for (int i=0; i<req.size();i++){
+            total += req.get(i).getNumRequests();
+        }
+        int average = total/req.size();
 
+        for (int i=0; i<req.size();i++){
+            if (req.get(i).getNumRequests()>average){
+                if ((cach.get(currentCache).size - cach.get(currentCache).fullness) > req.get(i).getVid().getSize() ){
+                    cach.get(currentCache)
+                }
+            }
+        }
 
 
     }
