74c8f26cc858d9b27377ada99ab4d82255cbc3a7
Gianluca Truda <gianlucatruda@gmail.com>
2017-02-23 22:26:39 +0200

    Small updates to HashCache methods

--- src/HashCache.java
+++ src/HashCache.java
@@ -5,9 +5,13 @@ public class HashCache {
     public int ID;
     public ArrayList<HashVideo> videos = new ArrayList<>();
 
-    public HashCache(int id, ArrayList<HashVideo> vids) {
+    public HashCache(int id, int s) {
         this.ID = id;
-        videos = new ArrayList<HashVideo>(5000);
+        this.size = s;
+
+    }
+
+    public void setVideos(ArrayList<HashVideo> vids) {
         for (HashVideo v : vids) {
             videos.add(v);
         }
