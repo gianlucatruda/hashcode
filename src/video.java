d2afd98e23f6e46628a10c26f358676bc081ccf2
Gianluca Truda <gianlucatruda@gmail.com>
2017-02-23 21:23:59 +0200

    Added ID variable to Video class

--- src/video.java
+++ src/video.java
@@ -1,11 +1,21 @@
 public class Video {
     public int size; //Size of video in MB.
+    public int ID; // The ID of the video.
 
-    public Video(int s) { // s is the size passed in at construction.
+    public Video(int id, int s) { // s is the size passed in at construction.
+        this.ID = id;
         this.size = s;
     }
 
     public int getSize() {
         return size;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
     }
 }
