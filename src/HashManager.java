55b65b0ccbe07d738123a24a10211e3dab19aaa7
Gianluca Truda <gianlucatruda@gmail.com>
2017-02-23 22:45:32 +0200

    Fleshed out skeleton of HashManager and added HashOptimiser

--- src/HashManager.java
+++ src/HashManager.java
@@ -1,5 +1,29 @@
+import java.util.ArrayList;
+
 public class HashManager {
     public static void main(String[] args) {
         System.out.println("hello world.");
+
+        String INFILE = "me_at_the_zoo.in";
+        String OUTFILE = "me_at_the_zoo.out";
+
+        // Create ArrayLists.
+        ArrayList<HashCache> caches;
+        ArrayList<HashEndpoint> endpoints;
+        ArrayList<HashRequest> requests;
+        ArrayList<HashVideo> videos;
+
+        // Read in from input file.
+        HashInputter inputter = new HashInputter(INFILE);
+        //some code to update the arraylists.
+
+        // Optimise caches.
+        HashOptimiser optimiser = new HashOptimiser();
+        //Some code to call optimiser.
+
+        // Generate output.
+        HashOutputter outputter = new HashOutputter(caches, OUTFILE);
+
+
     }
 }
---
+++ src/HashOptimiser.java
@@ -0,0 +1,7 @@
+public class HashOptimiser {
+
+    public HashOptimiser(){
+
+    }
+    
+}
