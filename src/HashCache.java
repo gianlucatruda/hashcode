fe3da562bea8fcc2cf021935ba7fc4c584578c94
Gianluca Truda <gianlucatruda@gmail.com>
2017-02-23 22:22:06 +0200

    Added ID variable to Cache

--- src/HashCache.java
+++ src/HashCache.java
@@ -2,10 +2,11 @@ import java.util.ArrayList;
 
 public class HashCache {
     public int size; // The size of the cache in MB.
+    public int ID;
     public ArrayList<HashVideo> videos = new ArrayList<>();
 
-    public HashCache(ArrayList<HashVideo> vids) {
-        //endpoints = new ArrayList(5000);
+    public HashCache(int id, ArrayList<HashVideo> vids) {
+        this.ID = id;
         videos = new ArrayList<HashVideo>(5000);
         for (HashVideo v : vids) {
             videos.add(v);
