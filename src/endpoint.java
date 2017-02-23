b9144152960da5a492b6b80dde146e9ec4f73772
Gianluca Truda <gianlucatruda@gmail.com>
2017-02-23 21:19:24 +0200

    Endpoints now hold arraylist of Caches

--- src/endpoint.java
+++ src/endpoint.java
@@ -1,8 +1,18 @@
+import java.util.ArrayList;
+
 public class Endpoint {
     public int dcLatency; // The latency to the datacenter.
+    public ArrayList caches;
+    public ArrayList cacheLatencies;
 
-    public Endpoint(int dcLat) {
+    public Endpoint(int dcLat, ArrayList cacheServers, ArrayList cLat) {
         this.dcLatency = dcLat;
+        for (Object c : cacheServers) {
+            caches.add(c);
+        }
+        for (Object l : cLat) {
+            cacheLatencies.add(l);
+        }
     }
 
     public int getDcLatency() {
